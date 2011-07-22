b9c6f0db4b65102bd2d2451f4721dfce8108e96b
nadeemabdulhamid <nadeem@acm.org>
2011-07-22 00:35:24 -0400

    updated info

--- info.rkt
+++ info.rkt
@@ -1,9 +1,10 @@
 #lang setup/infotab
 
 (define name "RacketUI")
-(define version "0.5.2")
+(define version "0.5.3")
 (define release-notes
   (list '(ul
+          (li "Fixed colonize bug in render")
           (li "Added documentation")
           (li "Now supports input/output files"))))
 (define repositories
